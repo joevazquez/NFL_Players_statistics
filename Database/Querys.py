68746389f84cab344cc25dea0d9e469a4252fe97
Joel Vázquez Anaya <54601149+joevazquez@users.noreply.github.com>
2024-10-15 12:17:28 -0600

    Top 10 of each statistic is added per year
    Se agrega el top 10 de cada tipo de estadística agrupadas por año

--- Database/Querys.py
+++ Database/Querys.py
@@ -7,61 +7,185 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Ahora se puede importar Config
 from Config import get_data
 
-# Definir la consulta SQL muestra todos los datos de la tabla Teams
+#Querys de validación de los datos
 query = "SELECT * FROM Teams"
-
-# Ejecutar la consulta
 df = get_data(query)
-
-# Imprimir los resultados
 if df.empty:
     print("La tabla 'Teams' está vacía.")
 else:
     print(df)
-
-
 print("----------------------------------------------------------------------------------------")
 
-# Definir la consulta SQL muestra todos los datos de la tabla Type
 query = "SELECT * FROM Type"
-
-# Ejecutar la consulta
 df = get_data(query)
-
-# Imprimir los resultados
 if df.empty:
     print("La tabla 'Type' está vacía.")
 else:
     print(df)
+print("----------------------------------------------------------------------------------------")
+
+query = ('''SELECT * 
+         FROM Player_season''')
+df = get_data(query)
+if df.empty:
+    print("La tabla 'Player_season' está vacía.")
+else:
+    print(df)
+print("----------------------------------------------------------------------------------------")
+
+query = ('''SELECT * 
+         FROM Statistics ''')
+df = get_data(query)
+if df.empty:
+    print("La tabla 'Statistics' está vacía.")
+else:
+    print(df)
+print("----------------------------------------------------------------------------------------")
+
+"""
+    Saca el top 10 con respecto a las Yardas totales de cada uno de los jugadores y
+    se saca de todos los jugadores de este tipo de estadística el número total de yardas
+    se ordenan de forma desendente y se saca el top 10 haciendo la agrupación por año, así 
+    se asegura regresar el top 10 por año de cada tipo de estadística
+    
+"""
+
+print('Top 10 por año de Passing')
+query = ('''
+   WITH RankedData AS (
+        SELECT
+            ps.Name,
+            t.Team_name AS Team,
+            ps.Posicion AS Position,
+            ps.Anio AS Year,
+            SUM(s.Yards) AS Total_Yards,
+            SUM(s.ATT) AS Total_Attempts,
+            SUM(s.Touchdowns) AS Total_Touchdowns,
+            ROW_NUMBER() OVER (PARTITION BY ps.Anio ORDER BY SUM(s.Yards) DESC) AS rank
+        FROM
+            Statistics s
+        JOIN
+            Player_season ps ON s.Player_ID = ps.Player_ID
+        JOIN
+            Teams t ON ps.Team_ID = t.Team_ID
+        WHERE
+            s.Type_ID = 1
+        GROUP BY
+            ps.Anio, ps.Name, t.Team_name, ps.Posicion
+    )
+    SELECT
+        Name,
+        Team,
+        Position,
+        Year,
+        Total_Yards,
+        Total_Attempts,
+        Total_Touchdowns
+    FROM
+        RankedData
+    WHERE
+        rank <= 10
+    ORDER BY
+        Year, Total_Yards DESC;
+''')
+
+df = get_data(query)
+if df.empty:
+    print("No se puede mostrar la información")
+else:
+    print(df)
+
 
 print("----------------------------------------------------------------------------------------")
 
-# Definir la consulta SQL muestra todos los datos de la tabla de Player_season
-query = ('''SELECT * 
-         FROM Player_season
-         WHERE Name == "Jared Goff"''')
+print('Top 10 por año de Rushing')
+query = ('''
+   WITH RankedData AS (
+        SELECT
+            ps.Name,
+            t.Team_name AS Team,
+            ps.Posicion AS Position,
+            ps.Anio AS Year,
+            SUM(s.Yards) AS Total_Yards,
+            SUM(s.ATT) AS Total_Attempts,
+            SUM(s.Touchdowns) AS Total_Touchdowns,
+            ROW_NUMBER() OVER (PARTITION BY ps.Anio ORDER BY SUM(s.Yards) DESC) AS rank
+        FROM
+            Statistics s
+        JOIN
+            Player_season ps ON s.Player_ID = ps.Player_ID
+        JOIN
+            Teams t ON ps.Team_ID = t.Team_ID
+        WHERE
+            s.Type_ID = 2
+        GROUP BY
+            ps.Anio, ps.Name, t.Team_name, ps.Posicion
+    )
+    SELECT
+        Name,
+        Team,
+        Position,
+        Year,
+        Total_Yards,
+        Total_Attempts,
+        Total_Touchdowns
+    FROM
+        RankedData
+    WHERE
+        rank <= 10
+    ORDER BY
+        Year, Total_Yards DESC;
+''')
 
-# Ejecutar la consulta
 df = get_data(query)
-
-# Imprimir los resultados
 if df.empty:
-    print("La tabla 'Player_season' está vacía.")
+    print("No se puede mostrar la información")
 else:
     print(df)
 
 print("----------------------------------------------------------------------------------------")
 
-# Definir la consulta SQL muestra todos los datos de la tabla Type
-query = ('''SELECT * 
-         FROM Statistics 
-         WHERE Name == "Jared Goff"''')
+print('Top 10 por año de Reciving')
+query = ('''
+   WITH RankedData AS (
+        SELECT
+            ps.Name,
+            t.Team_name AS Team,
+            ps.Posicion AS Position,
+            ps.Anio AS Year,
+            SUM(s.Yards) AS Total_Yards,
+            SUM(s.ATT) AS Total_Attempts,
+            SUM(s.Touchdowns) AS Total_Touchdowns,
+            ROW_NUMBER() OVER (PARTITION BY ps.Anio ORDER BY SUM(s.Yards) DESC) AS rank
+        FROM
+            Statistics s
+        JOIN
+            Player_season ps ON s.Player_ID = ps.Player_ID
+        JOIN
+            Teams t ON ps.Team_ID = t.Team_ID
+        WHERE
+            s.Type_ID = 3
+        GROUP BY
+            ps.Anio, ps.Name, t.Team_name, ps.Posicion
+    )
+    SELECT
+        Name,
+        Team,
+        Position,
+        Year,
+        Total_Yards,
+        Total_Attempts,
+        Total_Touchdowns
+    FROM
+        RankedData
+    WHERE
+        rank <= 10
+    ORDER BY
+        Year, Total_Yards DESC;
+''')
 
-# Ejecutar la consulta
 df = get_data(query)
-
-# Imprimir los resultados
 if df.empty:
-    print("La tabla 'Statistics' está vacía.")
+    print("No se puede mostrar la información")
 else:
     print(df)
