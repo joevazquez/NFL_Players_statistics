68b86c86c800b22cf4bccec8078d96ef41c29e07
Joel Vázquez Anaya <54601149+joevazquez@users.noreply.github.com>
2024-10-16 17:19:30 -0600

    Add top 10 table
    Se agrega la tabla del top 10 de los jugadores por tipo de estadística y por año

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import dash
-from dash import dcc, html
+from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
@@ -56,9 +56,102 @@ query_receiving = """
     GROUP BY ps.Posicion, ps.Anio
 """
 
+query_passing_top = ('''
+    WITH RankedData AS (
+        SELECT
+            ps.Name,
+            t.Team_name AS Team,
+            ps.Posicion AS Position,
+            ps.Anio AS Year,
+            SUM(s.Yards) AS Total_Yards,
+            SUM(s.Touchdowns) AS Total_Touchdowns,
+            ROW_NUMBER() OVER (PARTITION BY ps.Anio ORDER BY SUM(s.Yards) DESC) AS rank
+        FROM
+            Statistics s
+        JOIN
+            Player_season ps ON s.Player_ID = ps.Player_ID
+        JOIN
+            Teams t ON ps.Team_ID = t.Team_ID
+        WHERE
+            s.Type_ID = 1
+        GROUP BY
+            ps.Anio, ps.Name, t.Team_name, ps.Posicion
+    )
+    SELECT
+        Name, Team, Position, Year, Total_Yards, Total_Touchdowns
+    FROM
+        RankedData
+    WHERE
+        rank <= 10
+    ORDER BY
+        Year, Total_Yards DESC;
+''')
+
+query_rushing_top = ('''
+    WITH RankedData AS (
+        SELECT
+            ps.Name,
+            t.Team_name AS Team,
+            ps.Posicion AS Position,
+            ps.Anio AS Year,
+            SUM(s.Yards) AS Total_Yards,
+            SUM(s.Touchdowns) AS Total_Touchdowns,
+            ROW_NUMBER() OVER (PARTITION BY ps.Anio ORDER BY SUM(s.Yards) DESC) AS rank
+        FROM
+            Statistics s
+        JOIN
+            Player_season ps ON s.Player_ID = ps.Player_ID
+        JOIN
+            Teams t ON ps.Team_ID = t.Team_ID
+        WHERE
+            s.Type_ID = 2
+        GROUP BY
+            ps.Anio, ps.Name, t.Team_name, ps.Posicion
+    )
+    SELECT
+        Name, Team, Position, Year, Total_Yards, Total_Touchdowns
+    FROM
+        RankedData
+    WHERE
+        rank <= 10
+    ORDER BY
+        Year, Total_Yards DESC;
+''')
+
+query_receiving_top = ('''
+    WITH RankedData AS (
+        SELECT
+            ps.Name,
+            t.Team_name AS Team,
+            ps.Posicion AS Position,
+            ps.Anio AS Year,
+            SUM(s.Yards) AS Total_Yards,
+            SUM(s.Touchdowns) AS Total_Touchdowns,
+            ROW_NUMBER() OVER (PARTITION BY ps.Anio ORDER BY SUM(s.Yards) DESC) AS rank
+        FROM
+            Statistics s
+        JOIN
+            Player_season ps ON s.Player_ID = ps.Player_ID
+        JOIN
+            Teams t ON ps.Team_ID = t.Team_ID
+        WHERE
+            s.Type_ID = 3
+        GROUP BY
+            ps.Anio, ps.Name, t.Team_name, ps.Posicion
+    )
+    SELECT
+        Name, Team, Position, Year, Total_Yards, Total_Touchdowns
+    FROM
+        RankedData
+    WHERE
+        rank <= 10
+    ORDER BY
+        Year, Total_Yards DESC;
+''')
+
 # Inicializar la aplicación Dash con título personalizado y favicon
 app = dash.Dash(__name__)
-app.title = "NFL Stats Dashboard"  # Cambia esto al nombre que deseas mostrar
+app.title = "Dashboard NFL"  # Cambia esto al nombre que deseas mostrar
 
 
 app._favicon = 'nfl_logo.ico'  # Asegúrate de que el archivo 'favicon.ico' esté en la carpeta 'assets'
@@ -80,6 +173,44 @@ app.layout = html.Div([
         html.Div(dcc.Graph(id='rushing-pie-chart'), style={'width': '33%', 'padding': '10px'}),
         html.Div(dcc.Graph(id='receiving-pie-chart'), style={'width': '33%', 'padding': '10px'}),
     ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'}),
+
+html.H1("Top 10 jugadores por estadística", style={'text-align': 'center'}),
+    
+    dcc.Dropdown(
+        id='stat-type',
+        options=[
+            {'label': 'Passing', 'value': 'passing'},
+            {'label': 'Rushing', 'value': 'rushing'},
+            {'label': 'Receiving', 'value': 'receiving'}
+        ],
+        value='passing',
+        style={'width': '50%', 'margin': '0 auto'}
+    ),
+
+    dcc.Dropdown(
+        id='year-filter',
+        placeholder='Select a Year',
+        style={'width': '50%', 'margin': '10px auto'}
+    ),
+    
+    dash_table.DataTable(
+        id='top-10-table',
+        columns=[
+            {"name": "Team", "id": "Team"},
+            {"name": "Position", "id": "Position"},
+            {"name": "Name", "id": "Name"},
+            {"name": "Total Yards", "id": "Total_Yards"},
+            {"name": "Total Touchdowns", "id": "Total_Touchdowns"}
+        ],
+        data=[],
+        style_table={'overflowX': 'auto', 'padding-bottom' : '25px',},
+        style_cell={
+            'textAlign': 'center',
+            'padding-bottom' : '5px',
+            'whiteSpace': 'normal',
+        },
+        page_size=10
+    ),  
 ])
 
 @app.callback(
@@ -144,6 +275,40 @@ def update_pie_charts(selected_year):
     fig_receiving.update_layout(showlegend=True, height=500)
 
     return fig_passing, fig_rushing, fig_receiving
+
+# Callback para actualizar el filtro de años basado en el tipo de estadísticas
+@app.callback(
+    Output('year-filter', 'options'),
+    Input('stat-type', 'value')
+)
+def update_year_filter_options(selected_type):
+    if selected_type == 'passing':
+        df = get_data(query_passing_top)
+    elif selected_type == 'rushing':
+        df = get_data(query_rushing_top)
+    else:
+        df = get_data(query_receiving_top)
+
+    years = df['Year'].unique()
+    return [{'label': year, 'value': year} for year in years]
+
+# Callback para actualizar la tabla basada en la selección del dropdown
+@app.callback(
+    Output('top-10-table', 'data'),
+    [Input('stat-type', 'value'), Input('year-filter', 'value')]
+)
+def update_table(selected_type, selected_year):
+    if selected_type == 'passing':
+        df = get_data(query_passing_top)
+    elif selected_type == 'rushing':
+        df = get_data(query_rushing_top)
+    else:
+        df = get_data(query_receiving_top)
+
+    if selected_year:
+        df = df[df['Year'] == selected_year]
+
+    return df.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
